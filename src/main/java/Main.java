7ddf734fedad7437ec7ce507a906e779a0e2e23d
Samuel Gesang <sgesang@mailo.com>
2024-05-03 16:33:46 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,5 +8,7 @@ public class Main {
         List<Integer> numbers = List.of(10, 21, 33, 14, 55, 36, 79, 18, 9, 10);
         Optional<Integer> result = numbers.stream().filter(number -> number % 2 == 0).peek(System.out::println).map(number -> number * 2).sorted().peek(System.out::println).reduce(Integer::sum);
         result.ifPresent(System.out::println);
+
+        System.out.println("test2 remote")
     }
 }
