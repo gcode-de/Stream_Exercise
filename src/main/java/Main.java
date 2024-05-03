2144f8d5beb2f8a6bfae261930f95cbdc9ed6ab3
Samuel Gesang <sgesang@mailo.com>
2024-05-03 12:18:35 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> numbers = List.of(10, 21, 33, 14, 55, 36, 79, 18, 9, 10);
+        Optional<Integer> result = numbers.stream().filter(number -> number % 2 == 0).peek(System.out::println).map(number -> number * 2).sorted().peek(System.out::println).reduce(Integer::sum);
+        result.ifPresent(System.out::println);
+    }
+}
